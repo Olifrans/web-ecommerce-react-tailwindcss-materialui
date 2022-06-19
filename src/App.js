9546479a763b8240f4dd3c4c57d2c5c5bd60b9f3
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-06-19 14:54:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="mt-5 ml-5">ECommerce  Web Shopping Cart React Tailwindcss Material Ui</h1>
+    </div>
+  );
+}
+
+export default App;
