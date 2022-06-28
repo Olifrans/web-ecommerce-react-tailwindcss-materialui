1792d3e7477ac145b937545218e91f0e7c6b17cd
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-06-28 17:03:44 -0300

    feat: CategoriaPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Home from "./pages/Home";
-
+import CategoriaPage from "./pages/CategoriaPage";
+//import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      <CategoriaPage />
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/pages/CategoriaPage.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Anuncio from "../componentes/Anuncio";
+import Footer from "../componentes/Footer";
+import NavBar from "../componentes/NavBar";
+import NewsLetters from "../componentes/NewsLetters";
+import Produtos from "../componentes/Produtos";
+
+const CategoriaPage = () => {
+  return (
+    <div>
+      <Anuncio />
+      <NavBar />
+      <div className="flex flex-col p-5">
+        <h1 className="text-[30px]">Men's</h1>
+        <div className="flex items-center justify-between mt-3">
+          <div className="flex mobile:flex-col">
+            <p>Filter by</p>
+            <select className="ml-3 border-2 border-silver mobile:ml-0">
+              <option selected disabled>
+                Size
+              </option>
+              <option>Small</option>
+              <option>Medium</option>
+              <option>Large</option>
+              <option>Extra Large</option>
+            </select>
+            <select className="ml-3 border-2 border-silver mobile:ml-0">
+              <option selected disabled>
+                Color
+              </option>
+              <option>Yellow</option>
+              <option>Blue</option>
+              <option>Red</option>
+              <option>Green</option>
+            </select>
+          </div>
+          <div className="flex mobile:flex-col mobile:items-end">
+            <p>Sort by</p>
+            <select className="ml-3 border-2 border-silver">
+              <option selected>Newest (first)</option>
+              <option>Oldest (first)</option>
+              <option>Price (asc)</option>
+              <option>Price (desc)</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <Produtos />
+      <NewsLetters />
+      <Footer />
+    </div>
+  );
+};
+
+export default CategoriaPage;
