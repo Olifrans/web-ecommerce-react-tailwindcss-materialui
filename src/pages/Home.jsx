00091fe66c46a259215461355c2a7492e8229ff0
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-06-27 22:18:29 -0300

    feat: Sliders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Anuncio from "../componentes/Anuncio";
 import NavBar from "../componentes/NavBar";
+import Sliders from "../componentes/Sliders";
 
 function Home() {
   return (
     <div className="teste">
       <Anuncio />
       <NavBar />
+      <Sliders />
     </div>
   );
 }
